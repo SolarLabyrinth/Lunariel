c045108bcdca497fdd4f44a01e4622409f1aa71f
SolarLabyrinth <thesolarlabyrinth@gmail.com>
2024-07-07 17:55:51 -0500

    stranger danger

--- Resources/world_stats.gd
+++ Resources/world_stats.gd
@@ -1,9 +1,28 @@
 class_name WorldStats extends Resource
 
-@export var current_level := 0
-@export var player_x := 100.0
+@export var current_level := 1
+@export var player_x := 110.0
 @export var player_face := 'right'
-@export var at_grace := false
-@export var at_door := false
 
-@export var keys := { 1: false, 2: false }
+@export var last_grace_level := 1
+@export var last_grace_x := 110.0
+@export var last_grace_face := 'right'
+
+@export var current_enemy_id := 0
+
+@export var keys := {
+    1: false,
+    2: false,
+}
+@export var enemies := reset_enemies()
+
+func reset_enemies() -> Dictionary:
+    return {
+        1: false,
+        2: true,
+        3: true,
+        4: true,
+        5: true,
+        6: true,
+        7: true,
+    }
--- Things/enemy.gd
+++ Things/enemy.gd
@@ -2,9 +2,13 @@ class_name Enemy extends Node2D
 
 @export var enemy_id := 0
 
-func _on_area_2d_area_entered(_area: Area2D) -> void:
-	# SceneManager.start_typing_test()
-	pass # Replace with function body.
+var world_stats: WorldStats = ResourceLoader.load("res://Resources/world_stats.tres")
 
-func _on_area_2d_area_exited(_area: Area2D) -> void:
+func _ready() -> void:
+	if world_stats.enemies.get(enemy_id):
+		show()
+
+func _on_area_2d_area_entered(_area: Area2D) -> void:
+	world_stats.current_enemy_id = enemy_id
+	# SceneManager.start_typing_test()
 	pass # Replace with function body.
