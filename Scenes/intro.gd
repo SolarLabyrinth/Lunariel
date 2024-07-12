2c848a65e7645a59e24121c34494036362285f10
SolarLabyrinth <thesolarlabyrinth@gmail.com>
2024-07-11 20:20:16 -0500

    center

--- Scenes/intro.gd
+++ Scenes/intro.gd
@@ -14,14 +14,14 @@ Lunariel's Light may grace this place once more.""",
 @onready var text: RichTextLabel = %Text
 
 func _ready() -> void:
-	text.text = intro_text.pop_front()
+	text.text = "[center]" + intro_text.pop_front()
 	pass
 
 func _input(event: InputEvent) -> void:
 	if (event.is_action("ui_accept") or event.is_action("sl_left_mouse")) and !event.is_echo() and event.is_pressed():
 		var new_text = intro_text.pop_front()
 		if new_text:
-			text.text = new_text
+			text.text = "[center]" + new_text
 		else:
 			SceneManager.show_level(1)
 	pass
